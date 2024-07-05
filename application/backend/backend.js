452f6663fcdf275d3380bafbbdd8e15f60000156
AbdulaiSeydi <abdulaisb@tamu.edu>
2024-07-04 21:17:32 -0500

    attempt 1

--- application/backend/backend.js
+++ application/backend/backend.js
@@ -7,7 +7,11 @@ dotenv.config();
 
 const ObjectId = mongoose.Types.ObjectId;
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 // Middleware to parse JSON bodies
 app.use(express.json());
 //Listening
