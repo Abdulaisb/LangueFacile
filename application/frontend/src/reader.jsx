0d056b0e6cacfa0ffcfd43c60a25b11d4c04777d
AbdulaiSeydi <abdulaisb@tamu.edu>
2024-07-09 11:20:52 -0500

    env issues

--- application/frontend/src/reader.jsx
+++ application/frontend/src/reader.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
-import { apiURL } from './config';
 import gear from './images/gear_icon.jpg'
 import Sentence from './sentence';
 import ShelfItem from './shelfItem';
+const apiURL = import.meta.env.VITE_APIURL;
 
 const Reader = ({userData}) => {
   //Article Title
